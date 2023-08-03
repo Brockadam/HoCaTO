734282da2b3dcc8dbdfabaa03c31a49f7fe0169e
brockadam <brockadam6@gmail.com>
2023-08-03 09:17:01 -0500

    Initial commit

---
+++ src/main/java/com/brock/HoCaTO/HoCaToApplication.java
@@ -0,0 +1,13 @@
+package com.brock.HoCaTO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoCaToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoCaToApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brock/HoCaTO/HoCaToApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brock.HoCaTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoCaToApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
