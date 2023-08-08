0c8ea74630ded2b4fbe12de93db6bdbd29fa2c03
brockadam <brockadam6@gmail.com>
2023-08-07 20:08:50 -0500

    Added code for an auth and login system. Bare bones for now

--- src/main/java/com/brock/HoCaTO/controller/AuthController.java
+++ src/main/java/com/brock/HoCaTO/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -24,13 +25,19 @@ public class AuthController {
 
     // handler method to handle home page request
     @GetMapping("/index")
-    public String home(){
+    public String home() {
         return "index";
+    }
+
+    // handler method to handle login request
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 
     // handler method to handle user registration form request
     @GetMapping("/register")
-    public String showRegistrationForm(Model model){
+    public String showRegistrationForm(Model model) {
         // create model object to store form data
         UserDto user = new UserDto();
         model.addAttribute("user", user);
@@ -41,15 +48,15 @@ public class AuthController {
     @PostMapping("/register/save")
     public String registration(@Valid @ModelAttribute("user") UserDto userDto,
                                BindingResult result,
-                               Model model){
+                               Model model) {
         User existingUser = userService.findUserByEmail(userDto.getEmail());
 
-        if(existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()){
+        if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
             result.rejectValue("email", null,
                     "There is already an account registered with the same email");
         }
 
-        if(result.hasErrors()){
+        if (result.hasErrors()) {
             model.addAttribute("user", userDto);
             return "/register";
         }
@@ -60,15 +67,9 @@ public class AuthController {
 
     // handler method to handle list of users
     @GetMapping("/users")
-    public String users(Model model){
+    public String users(Model model) {
         List<UserDto> users = userService.findAllUsers();
         model.addAttribute("users", users);
         return "users";
-    }
-
-    // handler method to handle login request
-    @GetMapping("/login")
-    public String login(){
-        return "login";
     }
 }
